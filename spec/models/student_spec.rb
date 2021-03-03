f1b5a00dc598dcdb4a5dfce27297b191b06d55e4
Karson <kars.kalt@gmail.com>
2021-03-03 10:36:03 -0500

    commit tree 054e37f2f923f4208190c616516b63228d3c404f

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -5,10 +5,6 @@ describe Student do
     @student = Student.create!(first_name: "Daenerys", last_name: "Targaryen")
   end
 
-  it 'can be created' do
-    expect(@student).to be_valid
-  end
-
   it 'has a to_s instance method' do
     expect(@student.to_s).to eq("Daenerys Targaryen")
   end
